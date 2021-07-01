fdb2c2a6d44d34416fabfaba524c5a11dd08ee56
guiwitz <guiwitz@users.noreply.github.com>
2021-07-01 22:22:51 +0200

    update to handle multi-roi

--- splitmask/splitmask.py
+++ splitmask/splitmask.py
@@ -3,6 +3,7 @@ import itertools
 import skimage.measure
 import skimage.io
 import numpy as np
+import xarray as xr
 import matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -32,10 +33,39 @@ def get_roi_cm(roi_path=None, roi_im=None):
     else:
         roi_im = skimage.io.imread(roi_path)
     
+    roi_im = skimage.measure.label(roi_im)
     roi_props = skimage.measure.regionprops_table(roi_im, properties=('label','centroid'))
-    cm = [int(roi_props['centroid-0']),int(roi_props['centroid-1'])]
-
+    cm = np.stack([roi_props['centroid-0'].astype(np.uint16),roi_props['centroid-1'].astype(np.uint16)],axis=1)
+    
     return cm
+
+
+def save_labelled_roi(file_path, roi_path=None, roi_im=None):
+    """
+    Save an image of the labelled roi
+
+    Parameters
+    ----------
+    file_path: str of Path
+    roi_path: str
+        path to roi image
+    roi_im = 2d array
+        image containing roi
+
+    Returns
+    -------
+
+    """
+    
+    if roi_path is None:
+        if roi_im is None:
+            raise Exception("You have to provide an image if you don't provide a path")              
+    else:
+        roi_im = skimage.io.imread(roi_path)
+    
+    roi_im = skimage.measure.label(roi_im).astype(np.uint8)
+    skimage.io.imsave(file_path, roi_im, check_contrast=False)
+
 
 def create_concentric_mask(center, im_dims, sector_width=10, num_sectors=10):
     """
@@ -43,8 +73,8 @@ def create_concentric_mask(center, im_dims, sector_width=10, num_sectors=10):
 
     Parameters
     ----------
-    center: list
-        2d position of center of disk
+    center: list or array
+        2d position of center of disk(s)
     im_dims: list
         image size
     sector_width: int
@@ -54,22 +84,30 @@ def create_concentric_mask(center, im_dims, sector_width=10, num_sectors=10):
 
     Returns
     -------
-    concentric_labels: 2d array
+    concentric_labels: 3d array
         labelled image with concentric rings
+        each plane [x,:,:] corresponds to a roi
 
     """
     
+    if isinstance(center, list):
+        center = np.array([center])
+
     yy, xx = np.meshgrid(np.arange(im_dims[1]),np.arange(im_dims[0]))
-    roi_mask = np.zeros(im_dims, dtype=np.bool_)
-    concentric_masks = [roi_mask]
+    
+    concentric_labels = np.zeros((center.shape[0], im_dims[0], im_dims[1]), dtype=np.uint16)
+    
+    for r in range(center.shape[0]):
+        roi_mask = np.zeros(im_dims, dtype=np.bool_)
+        concentric_masks = [roi_mask]
 
-    for ind, i in enumerate(np.arange(sector_width, sector_width*num_sectors+1, sector_width)):
+        for ind, i in enumerate(np.arange(sector_width, sector_width*num_sectors+1, sector_width)):
 
-        temp_roi = np.sqrt((xx - center[0])**2 + (yy - center[1])**2) < i
-        concentric_masks.append((ind+1)*(temp_roi*~roi_mask))
-        roi_mask = temp_roi
+            temp_roi = np.sqrt((xx - center[r,0])**2 + (yy - center[r,1])**2) < i
+            concentric_masks.append((ind+1)*(temp_roi*~roi_mask))
+            roi_mask = temp_roi
 
-    concentric_labels = np.sum(np.array(concentric_masks),axis=0)
+        concentric_labels[r,:,:] = np.sum(np.array(concentric_masks),axis=0)
     
     return concentric_labels
 
@@ -94,24 +132,31 @@ def create_sector_mask(center, im_dims, angular_width=20, max_rad=50, ring_width
 
     Returns
     -------
-    sector_labels: 2d array
-        labelled image with disk split in sectors
+    sector_labels: 3d array
+        labelled image with concentric rings
+        each plane [x,:,:] corresponds to a roi
 
     """
 
-    yy, xx = np.meshgrid(np.arange(im_dims[1]),np.arange(im_dims[0]))
-    angles = np.arctan2(xx-center[0],yy-center[1])
-    angles %= (2*np.pi)
-    rad_mask = np.sqrt((xx - center[0])**2 + (yy - center[1])**2)
-    
-    if ring_width is None:
-        rad_mask = rad_mask < max_rad
-    else:
-        rad_mask = (rad_mask < max_rad) & (rad_mask > max_rad-ring_width)
+    if isinstance(center, list):
+        center = np.array([center])
 
-    sector_masks = [rad_mask*(ind+1)*((angles >= np.deg2rad(i)) *(angles < np.deg2rad(i+angular_width)))
-                    for ind, i in enumerate(np.arange(0,360-angular_width+1,angular_width))]
-    sector_labels = np.sum(np.array(sector_masks),axis=0)
+    yy, xx = np.meshgrid(np.arange(im_dims[1]),np.arange(im_dims[0]))
+
+    sector_labels = np.zeros((center.shape[0], im_dims[0], im_dims[1]), dtype=np.uint16)
+    for r in range(center.shape[0]):
+        angles = np.arctan2(xx-center[r,0],yy-center[r,1])
+        angles %= (2*np.pi)
+        rad_mask = np.sqrt((xx - center[r,0])**2 + (yy - center[r,1])**2)
+        
+        if ring_width is None:
+            rad_mask = rad_mask < max_rad
+        else:
+            rad_mask = (rad_mask < max_rad) & (rad_mask > max_rad-ring_width)
+
+        sector_masks = [rad_mask*(ind+1)*((angles >= np.deg2rad(i)) *(angles < np.deg2rad(i+angular_width)))
+                        for ind, i in enumerate(np.arange(0,360-angular_width+1,angular_width))]
+        sector_labels[r,:,:] = np.sum(np.array(sector_masks),axis=0)
 
     return sector_labels
 
@@ -133,79 +178,97 @@ def nan_labels(im_label):
     
     return im_label_nan
 
-def measure_intensities(time_image, im_labels, min_time=0, max_time=None, step=1):
+def measure_intensities(time_image, im_labels, channels, min_time=0, max_time=None, step=1):
     """
     Measure average intensity in a time-lapse image using a labelled image
 
     Parameters
     ----------
-    time_image: iterable 2d array
-        can be a TxHxW numpy array or iterable generating HxW numpy arrays
-    im_labels: 2d array
-        labelled image
+    time_image: array or microfilm.dataset
+        should be a 3D TxHxW numpy array or any microfilm.dataset object
+    im_labels: 3d array
+        labelled image with dimension RxHxW where R is for rois
+    channels: str or list of str
+        name(s) of channels to analyze
+    min_time: int
+        first time point to consider
     max_time: int
         last time point to consider
+    step: int
+        step between time points
 
     Returns
     -------
-    signal: 2d array
-        TxL array with mean intensity for each time point T in each labelled
-        region L
+    signal: 4d xarray
+        signal array with dimensions TxSxCxR where
+        T=time, S=splits, C=channels, R=rois
 
     """
 
-    measures = []
-    time_image_part = itertools.islice(time_image, min_time, max_time, step)
+    if max_time is None:
+        if isinstance(time_image, np.ndarray):
+            max_time = time_image.shape[0]
+        else:
+            max_time = time_image.max_time
 
-    for im_np in time_image_part:
-        measures.append(skimage.measure.regionprops_table(im_labels, 
-                                                          intensity_image=im_np, properties=('label','mean_intensity')))
+    time_gen = time_image.frame_generator(channel=channels)
+    time_image_part = itertools.islice(time_gen, min_time, max_time, step)
+    
+    num_channels = len(channels) if isinstance(channels, list) else 1
 
-    if im_np.ndim == 3:
-        signals = [np.stack([x['mean_intensity-'+str(k)] for x in measures],axis=0) for k in range(im_np.shape[2])]
-        signal = np.stack(signals, axis=2)
-    else:
-        signal = np.stack([x['mean_intensity'] for x in measures],axis=0)
+    signal = np.zeros((len(range(min_time, max_time, step)), im_labels.max(), num_channels, im_labels.shape[0]))
+
+    for t, im_np in enumerate(time_image_part):
+        for r in range(im_labels.shape[0]):
+            measures = skimage.measure.regionprops_table(im_labels[r], 
+                                                            intensity_image=im_np, properties=('label','mean_intensity'))        
+            if im_np.ndim == 3:
+                signal[t,:,:,r] = np.stack([measures['mean_intensity-'+str(k)] for k in range(im_np.shape[2])], axis=1)
+            else:
+                signal[t,:,0,r] = measures['mean_intensity']
+    
+    signal = xr.DataArray(signal, dims=("time", "sector", "channel", "roi"))
     
     return signal
 
-def plot_signals(signal, color_array=None, ax=None):
+def plot_signals(signal, channel=0, roi=0, color_array=None, ax=None):
     """
     Plot extracted signal with specific colormap
     
     Parameters
     ----------
-    signal: 2d array
-        signal array with rows as time points and columns as sectors
+    signal: 4d xarray
+        signal array with dimensions TxSxCxR where
+        T=time, S=splits, C=channels, R=rois
     color_array: 2d array
         N x 4 array of RGBA colors where N >= sectors
     ax: Matplotlib axis
 
     Returns
     -------
-    ax if ax passed as input otherwise fig
+    fig: Matplotlib figure
     
     """
 
-    fig = None
+    signal_sel = signal.sel(channel=channel, roi=roi)
     if ax is None:
         fig, ax = plt.subplots(figsize=(6,6))
-    for i in range(signal.shape[1]):
+    else:
+        fig = ax.figure
+    for i in range(signal_sel.shape[1]):
         if color_array is not None:
-            ax.plot(signal[:,i], color=color_array[i])
+            ax.plot(signal_sel[:,i], color=color_array[i])
         else:
-            ax.plot(signal[:,i])
+            ax.plot(signal_sel[:,i])
     ax.set_xlabel('Time', fontsize=20)
     ax.set_ylabel('Intensity', fontsize=20)
     ax.tick_params(axis='both', which='major', labelsize=15)
     
-    if fig is not None:
-        fig.tight_layout()
-        return fig
+    fig.tight_layout()
     
-    return ax
-
-def plot_sectors(image, sectors, channel=None, time=0, cmap=None, im_cmap=None, ax=None):
+    return fig
+    
+def plot_sectors(image, sectors, channel=None, time=0, roi=0, cmap=None, im_cmap=None, ax=None):
     """
     Plot image and overlayed sectors with a given colormap
     
@@ -213,12 +276,14 @@ def plot_sectors(image, sectors, channel=None, time=0, cmap=None, im_cmap=None, 
     ----------
     image: dataset object
         image to be plotted
-    sectors: 2d array
-        labelled image of sectors
+    sectors: 3d array
+        labelled image of sectors RxHxW, first dimension can contains multiple rois
     channel: str
         name of channel to plot
     time: int
         frame to plot
+    roi: int
+        index of roi to plot
     cmap: Matplotlib colormap
         colormap for split mask
     im_cmap: Matplotlib colormap
@@ -227,7 +292,7 @@ def plot_sectors(image, sectors, channel=None, time=0, cmap=None, im_cmap=None, 
 
     Returns
     -------
-    ax if ax passed as input otherwise fig
+    fig: Matplotlib figure
     
     """
     
@@ -237,42 +302,46 @@ def plot_sectors(image, sectors, channel=None, time=0, cmap=None, im_cmap=None, 
     fig = None
     if ax is None:
         fig, ax = plt.subplots(figsize=(6,6))
+    else:
+        fig = ax.figure
     if channel is None:
         channel = image.channel_name[0]
 
-    sector_labels_nan = nan_labels(sectors)
+    sector_labels_nan = nan_labels(sectors[roi])
 
-    ax.imshow(image.load_frame(channel,0), cmap=im_cmap)
+    ax.imshow(image.load_frame(channel,time), cmap=im_cmap)
     ax.imshow(sector_labels_nan, cmap=cmap, interpolation='none', alpha=0.5)
-    if fig is not None:
-        fig.tight_layout()
-        return fig
     
-    return ax
+    fig.tight_layout()
+    
+    return fig
 
-def save_signal(signal, name='mycsv.csv',format='long', channels=None):
+def save_signal(signal, name='mycsv.csv', channels=None):
     """
     Save the sector signal in a CSV file with a given name
 
     Parameters
     ----------
-    signal: array
-        signal array with rows as time points and columns as sectors
-        a third dimension indicates multiple channels
+    signal: 4d xarray
+        signal array with dimensions TxSxCxR where
+        T=time, S=splits, C=channels, R=rois
     name: str
         file name for export (should end in .csv)
-    format: str
-        'long' or 'wide'
-        in 'long' format, the table has three columns, time/sector/intensity
-        in 'wide' format, rows correspond to time and columns to sectors
 
     Returns
     -------
-    ax if ax passed as input otherwise fig
     
     """
     
-    if signal.ndim == 3:
+    signal_df = signal.to_dataframe(name='intensity').reset_index()
+
+    if channels is not None:
+        for ind, c in enumerate(channels):
+            signal_df.loc[signal_df['channel'] == ind, 'channel'] = c
+
+    signal_df.to_csv(name, index=False)
+
+    '''if signal.ndim == 3:
         format = 'long'
         if channels is None:
             channels = ['channel-'+str(i) for i in range(signal.shape[2])]
@@ -297,4 +366,4 @@ def save_signal(signal, name='mycsv.csv',format='long', channels=None):
                 dfs.append(temp_df)
             signal_df = pd.concat(dfs)
             
-        signal_df.to_csv(name, index=False)
+        signal_df.to_csv(name, index=False)'''
